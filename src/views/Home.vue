1c537eaf1181671b516cb8ebce8151bac6583bdd
MahassinPoree-El <poreeelm1@hawkmail.newpaltz.edu>
2018-12-20 11:15:05 -0500

    final commit(1)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,11 @@
       <h1 class="display-4">Welcome!</h1>
       <hr class="my-4">
       <a class="btn btn-primary btn-lg" @click.prevent="login" href="#" role="button">Log in with Facebook!</a>
+         <h1>Search Bar</h1>
+    <v-select :options="options" label="title">
+      <template slot="option" slot-scope="option">
+          <span class="fa" :class="option.icon"></span>
+          {{ option.title }}
     </div>
   </div>
 </template>
@@ -11,6 +16,9 @@
 <script>
 import * as api from '@/services/api_access';
 import * as fb from '@/services/facebook';
+import Vue from 'vue';
+import vSelect from './components/Select.vue';
+Vue.component('v-select', VueSelect.VueSelect)
 let loopTimer = null;
 export default {
 
@@ -19,6 +27,31 @@ export default {
       fb.FBLogin();
     },
     userId: ()=> api.userId
-}
-</script>
 
+      el: '#app',
+      data: {
+        options: [
+          {
+          title: 'About',
+          url: 'http://http://localhost:8080/about'
+          },
+          {
+            title: 'Share',
+            url: 'http://http://localhost:8080/share'
+          },
+          {
+            title: 'Profile',
+            url: 'http://http://localhost:8080/profile'
+          }
+
+
+
+        
+                ]// end of options
+        
+        
+              } //end of data
+        
+        
+  } // end of Vue object
+</script>
